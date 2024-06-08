00ea5a216bf937c59de60877840d2b41cbd50631
Alidevel <Alimerdanrahimov@gmail.com>
2024-06-08 15:53:25 +0000

    New reports

---
+++ barcode_print/barcode_print/report/customer_ledger_summary_report_piwo/customer_ledger_summary_report_piwo.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, alimerdanrahimov@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Customer Ledger Summary Report Piwo"] = {
+	"filters": [
+
+	]
+};
